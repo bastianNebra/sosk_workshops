2c498159304e82d72e3c093490b07f61a50442c3
bastianNebra <123bastianlontsi@gmail.com>
2025-01-06 11:15:33 +0100

    Delete Kuard Pod

--- routes/api.php
+++ routes/api.php
@@ -10,17 +10,6 @@ use App\Http\Controllers\OrderController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
 Route::post('register', [AuthController::class, 'register']);
 Route::post('login', [AuthController::class, 'login']);
 
@@ -37,7 +26,5 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::post('upload', [ImageController::class,"upload"]);
 
     Route::apiResource('orders', OrderController::class)->only('index','show');
-
-
 
 });
